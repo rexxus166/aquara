8089a0585320458a5ab77f329a67f8d705cd83be
Romi Setiawan <romi36825@gmail.com>
2025-12-23 06:59:42 +0700

    v2

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,9 +1,20 @@
 <?php
 // Sesuaikan dengan config website kamu
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db   = 'aquara';
+$serverName = $_SERVER['SERVER_NAME'];
+
+if ($serverName == 'localhost' || $serverName == '127.0.0.1') {
+    // LOCAL
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $db   = 'aquara';
+} else {
+    // PRODUCTION
+    $host = 'localhost';
+    $user = 'iqbfbjty_aquara';
+    $pass = 'Gz*N~)dSB9.?2;jC';
+    $db   = 'iqbfbjty_aquara';
+}
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 header('Content-Type: application/json');
@@ -12,4 +23,3 @@ if (!$koneksi) {
     echo json_encode(["message" => "Gagal Koneksi"]);
     exit();
 }
-?>
--- includes/config.php
+++ includes/config.php
@@ -4,10 +4,22 @@
   Konfigurasi koneksi database
 */
 
-define('DB_HOST', 'localhost');
-define('DB_USERNAME', 'iqbfbjty_aquara');
-define('DB_NAME', 'iqbfbjty_aquara');
-define('DB_PASSWORD', 'Gz*N~)dSB9.?2;jC');
+// Deteksi Environment
+$serverName = $_SERVER['SERVER_NAME'];
+
+if ($serverName == 'localhost' || $serverName == '127.0.0.1') {
+  // 1. LOCAL (Laragon/XAMPP)
+  define('DB_HOST', 'localhost');
+  define('DB_USERNAME', 'root');
+  define('DB_NAME', 'aquara');
+  define('DB_PASSWORD', '');
+} else {
+  // 2. PRODUCTION (Hosting)
+  define('DB_HOST', 'localhost');
+  define('DB_USERNAME', 'iqbfbjty_aquara');
+  define('DB_NAME', 'iqbfbjty_aquara');
+  define('DB_PASSWORD', 'Gz*N~)dSB9.?2;jC');
+}
 define('BASE_URL', 'https://aquara.miomidev.com');
 
 // Membuat koneksi
