4b380342eb6ebf8e669f7353b200fd35c39f33d3
Romi Setiawan <romi36825@gmail.com>
2025-12-23 07:03:13 +0700

    v3

--- includes/google_oauth_config.php
+++ includes/google_oauth_config.php
@@ -1,4 +1,6 @@
 <?php
 define('GOOGLE_CLIENT_ID', '555355946734-16v7l4ciivadcu9d2cmdc0f4nfk8dpod.apps.googleusercontent.com');
 define('GOOGLE_CLIENT_SECRET', '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');
-define('GOOGLE_REDIRECT_URI', 'http://localhost/aquara/views/google_callback.php');
+$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http";
+$host = $_SERVER['HTTP_HOST'];
+define('GOOGLE_REDIRECT_URI', $protocol . '://' . $host . '/aquara/views/google_callback.php');
