dc7fce6ec368530d09b9cb288c4eb9ff276cd1ad
Romi Setiawan <romi36825@gmail.com>
2025-12-23 07:07:07 +0700

    callback

--- includes/google_oauth_config.php
+++ includes/google_oauth_config.php
@@ -3,4 +3,8 @@ define('GOOGLE_CLIENT_ID', '555355946734-16v7l4ciivadcu9d2cmdc0f4nfk8dpod.apps.g
 define('GOOGLE_CLIENT_SECRET', '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');
 $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http";
 $host = $_SERVER['HTTP_HOST'];
-define('GOOGLE_REDIRECT_URI', $protocol . '://' . $host . '/aquara/views/google_callback.php');
+
+// Jika di Localhost, pakai folder '/aquara', jika di Hosting (subdomain) langsung '/views...'
+$path = ($host == 'localhost' || $host == '127.0.0.1') ? '/aquara' : '';
+
+define('GOOGLE_REDIRECT_URI', $protocol . '://' . $host . $path . '/views/google_callback.php');
